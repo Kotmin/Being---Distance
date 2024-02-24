97c5f779dbb7c1ec64228b096e24f1f2442f843e
kotmin <kotmin@users.noreply.github.com>
2024-02-24 09:48:49 +0100

    refactor to pydantic format

--- being-and-distance/db/Person.py
+++ being-and-distance/db/Person.py
@@ -5,12 +5,12 @@ class Person(Being,BaseModel):
         name: str
         total_distance_moved_by_person:int = 0
 
-    def move(self, distance: float | int) -> None:
-        self.total_distance_moved_by_person += distance
-        Being.total_distance_moved += distance
+        def move(self, distance: float | int) -> None:
+            self.total_distance_moved_by_person += distance
+            Being.total_distance_moved += distance
 
-    @validator('name')
-    def name_must_not_be_empty(cls, value):
-       if not value:
-             raise ValueError("Name cannot be empty")
-        return value
+        @validator('name')
+        def name_must_not_be_empty(cls, value):
+            if not value:
+                    raise ValueError("Name cannot be empty")
+            return value
